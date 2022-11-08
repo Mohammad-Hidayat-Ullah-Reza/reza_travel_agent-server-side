705eb07b744036da7ccf01b571fd8e84e5df4179
mohammadHidayat <mohammadhidayat8@gmail.com>
2022-11-08 18:31:01 +0600

    services/:id api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -33,8 +33,15 @@ async function run() {
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = fakeDataCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await fakeDataCollection.findOne(query);
+      res.send(service);
     });
   } finally {
   }
