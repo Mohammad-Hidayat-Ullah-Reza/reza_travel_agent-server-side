8524b3db7edfb2f4bb68e080d143d7354a66a5ca
mohammadHidayat <mohammadhidayat8@gmail.com>
2022-11-08 13:24:16 +0600

    enviroment variables added, mongodb connected, able to read last 3 data

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const cors = require("cors");
-const { Console } = require("console");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dp8wnte.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const fakeDataCollection = client.db("travelReview").collection("fakeData");
+
+    // get last 3 data only
+    app.get("/fake", async (req, res) => {
+      const query = {};
+      const cursor = fakeDataCollection
+        .find(query)
+        .sort({ _id: -1 })
+        .limit(3, function (e, d) {});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.send("review server running");
